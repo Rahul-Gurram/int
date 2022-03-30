5c1826fdc3760614a46a05f077af28c2787fefa0
Rahul Gurram <98640253+Rahul-Gurram@users.noreply.github.com>
2022-03-30 12:46:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,51 +2,24 @@ import flask
 from flask import Flask, render_template, request, Markup
 import numpy as np
 import pandas as pd
-import requests
 import pickle
-
-# Use pickle to load in the pre-trained model
-with open(f'model/Insurance.pkl', 'rb') as f:
-    model = pickle.load(f)
 
 # Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
 
-# Set up the main route
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'get':
-        # Just render the initial form, to get input
-        return(flask.render_template('main.html'))
-    
-    if flask.request.method == 'POST':
-        # Extract the input
-        age = int(request.form['age'])
-        sex = str(request.form['sex'])
-        bmi = int(request.form['bmi'])
-        children = int(request.form['children'])
-        smoker = str(request.form['smoker'])
-        region = str(request.form['region'])
+# prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 12)
+    loaded_model = pickle.load(open(f'model/Insurance.pkl', "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+ 
+@app.route('/', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)       
+        return render_template("main.html", prediction = result)
 
-        # Make DataFrame for model
-        input_variables = pd.DataFrame([[age, sex, bmi, children, smoker, region]],
-                                       columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region'],
-                                       index=['input'])
-
-        # Get the model's prediction
-        prediction = model.predict(input_variables)
-    
-        # Render the form again, but add in the prediction and remind user
-        # of the values they input before
-        return flask.render_template('main.html',
-                                     original_input={'Age':age,
-                                                     'Sex':sex,
-                                                     'BMI':bmi,
-                                                     'Children':children,
-                                                     'Smoker':smoker,
-                                                     'Region':region},
-                                     result=prediction,
-                                     )
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
